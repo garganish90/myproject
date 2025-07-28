0a37b6caa2d1216a56be5b524b479d895245662d
Anish Garg <garganish9090@gmail.com>
2025-07-28 14:24:11 +0530

    Update Find.jsx

--- client/src/components/Find.jsx
+++ client/src/components/Find.jsx
@@ -12,7 +12,7 @@ export function Find(){
         e.preventDefault();
         try
         {
-const response = await axios.get(`http://localhost:3001/api/employees/${id}`);
+const response = await axios.get(`https://anish-employee.onrender.com/api/employees/${id}`);
 setEmployee(response.data);
         }
         catch(err)
